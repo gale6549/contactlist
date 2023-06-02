a11942fd558b5004e4b38cbe42b58b9b73fe9e1f
gale6549 <gale6549@gmail.com>
2023-06-01 22:07:59 -0700

    first commit

---
+++ src/component/ContactList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ContactCard from './ContactCard';
+
+const ContactList = (props) => {
+    console.log(props);
+
+    const deleteContactHandler=(id)=> {
+      props.getContactId(id);
+    };
+    const renderContactList=props.contacts.map((contacts)=>{
+        return <ContactCard contacts={contacts} clickHandler= {deleteContactHandler} />
+
+    });
+  return <div className="ui celled list">{renderContactList}
+    </div>
+};
+
+export default ContactList;
